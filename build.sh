239b9e1302cced339b81ed8bd3ce177672b80e5c
ArtikusHG <24320212+ArtikusHG@users.noreply.github.com>
2023-09-09 12:23:36 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 data=$(curl -s https://divestos.org/fdroid/official/index-v1.json)
-hash=$(echo $data | jq -r '.packages."us.spotco.fennec_dos"[0].hash')
-if echo "$hash latest.apk" | sha256sum -c -; then
-	echo "Latest apk already patched"
-	exit 1
-fi
-rm latest.apk
 apk=$(echo $data | jq -r '.packages."us.spotco.fennec_dos"[0].apkName')
-echo "https://divestos.org/fdroid/official/$apk"
 wget $(echo "https://divestos.org/fdroid/official/$apk") -O latest.apk
-
-ls
-pwd
 
 echo "Decompiling and patching apk file..."
 rm -rf patched patched_signed.apk
@@ -20,6 +10,8 @@ rm -rf patched/META-INF
 
 sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">@color\/photonBlack<\/color>/g' patched/res/values-night/colors.xml
 sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' patched/res/values-night/colors.xml
+
+aapt2 --help
 
 apktool b patched -o patched.apk -a /usr/bin/aapt2
 
