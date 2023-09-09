e0389c63a89bb4ebc476b3065f81e1bde0a97534
ArtikusHG <24320212+ArtikusHG@users.noreply.github.com>
2023-09-09 13:06:28 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 data=$(curl -s https://divestos.org/fdroid/official/index-v1.json)
 apk=$(echo $data | jq -r '.packages."us.spotco.fennec_dos"[0].apkName')
-wget $(echo "https://divestos.org/fdroid/official/$apk") -O latest.apk
+wget -q $(echo "https://divestos.org/fdroid/official/$apk") -O latest.apk
 
 echo "Decompiling and patching apk file..."
 rm -rf patched patched_signed.apk
@@ -11,7 +11,7 @@ rm -rf patched/META-INF
 sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">@color\/photonBlack<\/color>/g' patched/res/values-night/colors.xml
 sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' patched/res/values-night/colors.xml
 
-apktool b patched -o patched.apk --use-aapt2 -p ./tmp
+apktool b patched -o patched.apk --use-aapt2
 
 echo "Aligning apk file..."
 zipalign 4 patched.apk patched_signed.apk
