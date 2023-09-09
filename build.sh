ae4080141e42fcaa7e205189af981ab4458fd6f8
ArtikusHG <24320212+ArtikusHG@users.noreply.github.com>
2023-09-09 13:15:33 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ wget -q $(echo "https://divestos.org/fdroid/official/$apk") -O latest.apk
 
 echo "Decompiling and patching apk file..."
 rm -rf patched patched_signed.apk
-apktool d -s latest.apk -o patched 
+./apktool d -s latest.apk -o patched 
 rm -rf patched/META-INF
 
 sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">@color\/photonBlack<\/color>/g' patched/res/values-night/colors.xml
 sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' patched/res/values-night/colors.xml
 
-apktool b patched -o patched.apk --use-aapt2
+./apktool b patched -o patched.apk --use-aapt2
 
 echo "Aligning apk file..."
 zipalign 4 patched.apk patched_signed.apk
