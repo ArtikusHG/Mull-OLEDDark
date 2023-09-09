99cbd90d8a238ec3ea131ed69b6d4cf09b761f20
ArtikusHG <24320212+ArtikusHG@users.noreply.github.com>
2023-09-09 13:18:11 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@ data=$(curl -s https://divestos.org/fdroid/official/index-v1.json)
 apk=$(echo $data | jq -r '.packages."us.spotco.fennec_dos"[0].apkName')
 wget -q $(echo "https://divestos.org/fdroid/official/$apk") -O latest.apk
 
-echo "Decompiling and patching apk file..."
+wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.8.1.jar -O apktool.jar
+wget -q https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool
+chmod +x apktool*
+
 rm -rf patched patched_signed.apk
 ./apktool d -s latest.apk -o patched 
 rm -rf patched/META-INF
@@ -13,6 +16,5 @@ sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_
 
 ./apktool b patched -o patched.apk --use-aapt2
 
-echo "Aligning apk file..."
 zipalign 4 patched.apk patched_signed.apk
 rm -rf patched patched.apk
