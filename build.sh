3090663c81785c49801d932c7b84a21fc051d425
ArtikusHG <24320212+ArtikusHG@users.noreply.github.com>
2025-01-21 17:46:21 +0000

    Update build.sh to Ironfox

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-data=$(curl -s https://divestos.org/fdroid/official/index-v1.json)
-apk=$(echo $data | jq -r '.packages."us.spotco.fennec_dos"[0].apkName')
-wget -q $(echo "https://divestos.org/fdroid/official/$apk") -O latest.apk
+data=$(curl -s https://fdroid.ironfoxoss.org/fdroid/repo/index-v1.json)
+apk=$(echo $data | jq -r '.packages."org.ironfoxoss.ironfox"[0].apkName')
+wget -q $(echo "https://fdroid.ironfoxoss.org/fdroid/repo/$apk") -O latest.apk
 
 wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.8.1.jar -O apktool.jar
 wget -q https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool
